1d19e9faa2524dafce6648efb8d72348b090093b
harshvegada <harshhpatel07@gmail.com>
2024-03-30 17:38:00 +0530

    Project Upload

---
+++ src/hiralPurohit/LaunchChrome.java
@@ -0,0 +1,15 @@
+package hiralPurohit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		driver.quit();
+	}
+
+}
