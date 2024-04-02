6e25abe8d3da5ab73e50fe2f6beb8bd218083fd9
sveta.kanani <sveta.kanani123@gmail.com>
2024-04-02 23:36:58 +0530

    Sveta_SeleniumAssignment_2

--- src/svetaKanani/FacebookForm.java
+++ src/svetaKanani/FacebookForm.java
@@ -16,19 +16,25 @@ public class FacebookForm {
 		//click on create account Button
 		driver.findElement(By.xpath("//div/a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
 		Thread.sleep(3000);
+		System.out.println("Enter FirstName....");
 		driver.findElement(By.xpath("//input[@placeholder='First name']")).sendKeys("Sveta");
+		System.out.println("Enter Surname....");
 		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("kanani");
+		System.out.println("Enter MobileNo or Email...");
 		driver.findElement(By.xpath("//input[@name='reg_email__']")).sendKeys("Sveta@gmail.com");
+		System.out.println("Enter Password");
 		driver.findElement(By.xpath("//input[@id='password_step_input']")).sendKeys("Sveta@123");
+		System.out.println("Select date..");
 		WebElement e= driver.findElement(By.xpath("//select[@id='day']"));
 		Select select = new Select(e);
 		select.selectByValue("30");
-		
+		System.out.println("Select month..");
 		driver.findElement(By.xpath("//select[@id='month']/option[@value='6']")).click();
-		
+		System.out.println("Select year..");
 		driver.findElement(By.xpath("//select[@id='year']/option[@value='2002']")).click();
-		
+		System.out.println("Select checkbox..");
 		driver.findElement(By.xpath("//input[@value='1']")).click();
+		System.out.println("close browser..");
 		driver.quit();
 	}
 	public static void main(String[] args) throws InterruptedException {
