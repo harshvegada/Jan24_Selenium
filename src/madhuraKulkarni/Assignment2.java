53b5e593b2be4ada54d84df2b40d41344ec6756e
madhurakulkarni009 <madhura.kulkarni009@gmail.com>
2024-04-03 16:21:17 +0530

    src/madhuraKulkarni/Assignment2.java

---
+++ src/madhuraKulkarni/Assignment2.java
@@ -0,0 +1,136 @@
+/*Assignment - 2 : 2nd April'2024
+
+Step 1 : Launch browser and hit https://facebook.com
+Step 2 : Click on Create Account button
+Step 3 : Enter appropriate value in all textbox. 
+Step 4 : Select Date from Date dropdown
+Step 5 : Select Month from month dropdown
+Step 6 : Select Year from year dropdown
+Step 7 : Verify Selected value of date with expected value
+Step 8 : Verify Selected value of month with expected value
+Step 9 : Verify Selected value of year with expected value  */
+package madhuraKulkarni;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+//import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Assignment2 {
+	WebDriver driver;
+
+	void launchBrowser() throws InterruptedException {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		System.out.println("Step 1 : Launch browser and hit https://facebook.com");
+		driver.get("https://www.facebook.com/");
+		System.out.println("Browser launched...");
+		System.out.println("Step 2 : Click on Create Account button");
+		driver.findElement(By.xpath("//a[@rel='async']")).click();
+		Thread.sleep(3000);
+		System.out.println("Clicked on Create new Account button...");
+
+	}
+
+	void verifyCreateNewAccount() throws InterruptedException {
+		System.out.println("Step 3 : Enter appropriate value in all textbox.");
+		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Madhura");
+		System.out.println("Entered first name...");
+		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Kulkarni");
+		System.out.println("Entered last name...");
+		driver.findElement(By.xpath("//input[@name='reg_email__']")).sendKeys("madhura.test@gmail.com");
+		System.out.println("Entered email address...");
+		driver.findElement(By.xpath("//input[@name='reg_email_confirmation__']")).sendKeys("madhura.test@gmail.com");
+		System.out.println("Re-entered email address...");
+		driver.findElement(By.xpath("//input[@id='password_step_input']")).sendKeys("madhura&test123");
+		System.out.println("Entered password...");
+	}
+
+	void verifyDayIsSelectedFromDropdown() {
+		System.out.println("Step 4 : Select Date from Date dropdown");
+		WebElement e = driver.findElement(By.xpath("//select[@id='day']"));
+		Select daySelect = new Select(e);
+		daySelect.selectByIndex(4);
+		System.out.println("Entered Day...");
+		List<WebElement> dayOptionSelect = daySelect.getOptions();
+		String daySelected = "";
+		for (WebElement element : dayOptionSelect) {
+			if (element.isSelected()) {
+				System.out.println(element.getText());
+				daySelected = element.getText();
+			}
+		}
+		System.out.println("Step 5 : Verify Selected value of date with expected value");
+		if (daySelected.equals("5")) {
+			System.out.println("Verified the selected day from dropdown...");
+			System.out.println("Test passed...");
+		} else {
+			System.out.println("Test failed...");
+		}
+	}
+
+	void verifyMonthSeectedFromDropdown() {
+		System.out.println("Step 6 : Select Month from month dropdown");
+		WebElement e = driver.findElement(By.xpath("//select[@id='month']"));
+		Select monthSelect = new Select(e);
+		monthSelect.selectByVisibleText("Aug");
+		System.out.println("Entered month...");
+		List<WebElement> monthOptionSelect = monthSelect.getOptions();
+		String monthSelected = "";
+		for (WebElement element : monthOptionSelect) {
+			if (element.isSelected()) {
+				System.out.println(element.getText());
+				monthSelected = element.getText();
+			}
+		}
+		System.out.println("Step 7 : Verify Selected value of month with expected value");
+		if (monthSelected.equals("Aug")) {
+			System.out.println("Verified the selected month from dropdown...");
+			System.out.println("Test passed...");
+		} else {
+			System.out.println("Test failed...");
+		}
+	}
+
+	void verifyIsYearSelectedFromDropdown() {
+		System.out.println("Step 8 : Select Month from year dropdown");
+		WebElement e = driver.findElement(By.xpath("//select[@id='year']"));
+		Select yearSelect = new Select(e);
+		yearSelect.selectByValue("1990");
+		System.out.println("Entered year...");
+		List<WebElement> yearOptionSelect = yearSelect.getOptions();
+		String yearSelected = "";
+		for (WebElement element : yearOptionSelect) {
+			if (element.isSelected()) {
+				System.out.println(element.getText());
+				yearSelected = element.getText();
+			}
+		}
+		System.out.println("Step 9 : Verify Selected value of year with expected value");
+		if (yearSelected.equals("1990")) {
+			System.out.println("Verified the selected year from dropdown...");
+			System.out.println("Test passed...");
+		} else {
+			System.out.println("Test failed...");
+		}
+	}
+
+	void displayData() throws InterruptedException {
+		launchBrowser();
+		verifyCreateNewAccount();
+		verifyDayIsSelectedFromDropdown();
+		verifyMonthSeectedFromDropdown();
+		verifyIsYearSelectedFromDropdown();
+		driver.quit();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new Assignment2().displayData();
+	}
+}
