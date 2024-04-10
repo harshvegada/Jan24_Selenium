62aedc9f0c5f03a16eff7196060ec60d02533311
Garima <garimasinghal2285@gmail.com>
2024-04-10 16:29:34 +0530

    GarimaS_Assignment_8

--- src/garimaSinghal/Assignment_8.java
+++ src/garimaSinghal/Assignment_8.java
@@ -1,8 +1,6 @@
 package garimaSinghal;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -50,38 +48,15 @@ public class Assignment_8 {
 			}
 		}
 		System.out.println("ManagerId " + output + " having Maximum Mentors");
-	}
-
-	void testCase2() {
-		int row = driver.findElements(By.xpath("//table[@class='table table-striped']/tbody/tr")).size();
-		Map<String, String> map = new HashMap<String, String>();
-		for (int i = 1; i <= row; i++) {
-			String mngId = driver
-					.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr[" + i + "]/td[4]")).getText();
-			String empname = driver
-					.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr[" + i + "]/td[3]")).getText();
-			if (map.containsKey(mngId)) {
-				String temp = map.get(mngId) + "," + empname;
-				map.put(mngId, temp);
-			} else {
-				map.put(mngId, empname);
-			}
-		}
-		System.out
-				.println("STEP : Get Manager IDs with number of counts and name of employees working in each Manager");
-		Set<String> setOfMngIds = map.keySet();
-		for (String currentMngId : setOfMngIds) {
-			String empList = map.get(currentMngId);
-			System.out.println(currentMngId + "--->" + empList.split(",").length + " = " + empList);
-		}
 		driver.quit();
 	}
+
+	
 
 	public static void main(String[] args) {
 		Assignment_8 assignment_8 = new Assignment_8();
 		System.out.println("==============================");
 		assignment_8.testCase1();
-		System.out.println("==============================");
-		assignment_8.testCase2();
+		
 	}
 }
