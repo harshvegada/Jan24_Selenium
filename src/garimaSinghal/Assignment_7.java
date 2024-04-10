6a193e705ff2738b75b3f03e4249e4b532f453dc
Garima <garimasinghal2285@gmail.com>
2024-04-10 15:19:58 +0530

    GarimaS_Assignment_7

---
+++ src/garimaSinghal/Assignment_7.java
@@ -0,0 +1,74 @@
+package garimaSinghal;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment_7 {
+	WebDriver driver;
+
+	void setUp() {
+		driver = new ChromeDriver();
+		System.out.println("STEP - Launch chrome browser and hit automationbykrishna.com");
+		driver.get("http://automationbykrishna.com");
+		driver.manage().window().maximize();
+		System.out.println("STEP - click on Demo link");
+		driver.findElement(By.xpath("//a[@id='demotable']")).click();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	}
+
+	void testCase1() {
+		setUp();
+		int row = driver.findElements(By.xpath("//table[@class='table table-striped']/tbody/tr/td[5]")).size();
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 1; i <= row; i++) {
+			String depttname = driver
+					.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr[" + i + "]/td[5]")).getText();
+			if (map.containsKey(depttname)) {
+				int count = map.get(depttname) + 1;
+				map.put(depttname, count);
+			} else {
+				map.put(depttname, 1);
+			}
+		}
+		System.out.println("STEP : Get department name and number of employees working in each department");
+		System.out.println(map);
+	}
+
+	void testCase2() {
+		int row = driver.findElements(By.xpath("//table[@class='table table-striped']/tbody/tr/td[5]")).size();
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
+		for (int i = 1; i <= row; i++) {
+			String depttname = driver
+					.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr[" + i + "]/td[5]")).getText();
+			String empname = driver
+					.findElement(By.xpath("//table[@class='table table-striped']/tbody/tr[" + i + "]/td[3]")).getText();
+			if (map.containsKey(depttname)) {
+				List<String> listOfEmp = map.get(depttname);
+				listOfEmp.add(empname);
+				map.put(depttname, listOfEmp);
+			} else {
+				List<String> listOfEmp = new LinkedList<String>();
+				listOfEmp.add(empname);
+				map.put(depttname, listOfEmp);
+			}
+		}
+		System.out.println("STEP : Get department name and name of employees working in each department");
+		System.out.println(map);
+		driver.quit();
+	}
+
+	public static void main(String[] args) {
+		Assignment_7 assignment_7 = new Assignment_7();
+		System.out.println("==============================");
+		assignment_7.testCase1();
+		System.out.println("==============================");
+		assignment_7.testCase2();
+	}
+}
