ed753e2cf7b0e664fda00fdf11e720b53281d16d
Akshay Gaikwad <akshaygaikwad1218@gmail.com>
2024-04-16 21:32:27 +0530

    Excel_reading_Updated

--- src/akshayGaikwad/ExcelFileReading.java
+++ src/akshayGaikwad/ExcelFileReading.java
@@ -2,46 +2,38 @@ package akshayGaikwad;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//import org.apache.poi.ss.usermodel.Sheet;
-//import org.apache.poi.ss.usermodel.Workbook;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.testng.annotations.Test;
 
 public class ExcelFileReading {
 	
-	static void getDataFromExcel() {
-		String filePath = ".\\Data.xlsx";
-		String sheetName = "LoginData";
+	static String[][] getDataFromExcel(String filePath, String sheetName) {
 		File file = new File(filePath);
+		Workbook wb;
+		String[][] dataArr = null;
 		try {
 			FileInputStream fileInputStream = new FileInputStream(file);
-			Workbook wb = new XSSFWorkbook(fileInputStream);
+			wb = new XSSFWorkbook(fileInputStream);
 			Sheet sheet = wb.getSheet(sheetName);
-			sheet.getRow(0).getCell(0).getStringCellValue();
-
 			int row = sheet.getLastRowNum();
 			int col = sheet.getRow(0).getLastCellNum();
 
-			String[][] dataArr = new String[row][col];
+			dataArr = new String[row][col];
 
 			for (int rowIndex = 1; rowIndex <= row; rowIndex++) {
 				for (int colIndex = 0; colIndex < col; colIndex++) {
 					dataArr[rowIndex-1][colIndex] = sheet.getRow(rowIndex).getCell(colIndex).getStringCellValue();
-					System.out.println(sheet.getRow(rowIndex).getCell(colIndex).getStringCellValue());
+					System.out.print(sheet.getRow(rowIndex).getCell(colIndex).getStringCellValue()+" ");
 				}
+				System.out.println();
 			}
 		} catch (Exception e) {
 			System.out.println("File not found at given location");
 		}
+		return dataArr;
 	}
-	
-	public static void main(String[] args) {
-		getDataFromExcel();
-	}
-	
-	
 }
