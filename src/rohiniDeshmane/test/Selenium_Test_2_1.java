6e7658d238f1a46381a5c692f247de1e36fbe829
Rohini Deshmane <Rohindeshmane.21@gmail.com>
2024-04-25 11:41:41 +0530

    RohiniD_Test_2

--- src/rohiniDeshmane/test/Selenium_Test_2_1.java
+++ src/rohiniDeshmane/test/Selenium_Test_2_1.java
@@ -1,6 +1,7 @@
 package rohiniDeshmane.test;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -54,20 +55,37 @@ public class Selenium_Test_2_1 {
 	
 	@Test
 	void TestCase1() {
+		
+		WebElement transparentBtn =driver.findElement(By.xpath("//button[@id='transparentButton']"));
+		System.out.println("Before click transparent btn visibility = "+transparentBtn.isDisplayed());
+		
+		WebElement invisibleButton =driver.findElement(By.xpath("//button[@id='invisibleButton']"));
+		System.out.println("Before click invisibleButton visibility = "+invisibleButton.isDisplayed());
+		
+		WebElement removedButton =driver.findElement(By.xpath("//button[@id='removedButton']"));
+		System.out.println("Before click removedButton visibility = "+removedButton.isDisplayed());
+		
+		WebElement notdisplayedButton =driver.findElement(By.xpath("//button[@id='notdisplayedButton']"));
+		System.out.println("Before click notdisplayedButton visibility = "+notdisplayedButton.isDisplayed());
+		
 		System.out.println("STEP : Click on Hide Button");
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='hideButton']"))).click();
 		
-		System.out.println("STEP : The button labeled Opacity 0 should not be displayed");
-		Assert.assertTrue(wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[@id='transparentButton']"))));
+		System.out.println("VERIFY : The button labeled Opacity 0 should not be displayed");
+		Assert.assertFalse(transparentBtn.isDisplayed(), "Transparent button is visible");
 		
-		System.out.println("STEP : The button labeled Removed should not be displayed");
-		Assert.assertTrue(wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[@id='removedButton']"))));
+		System.out.println("VERIFY : The button labeled Removed should not be displayed");
+		try {
+			Assert.assertFalse(removedButton.isDisplayed(), "removedButton is visible");
+		} catch (Exception e) {
+			System.out.println("RemovedButton Element not found");
+		}
 		
-		System.out.println("STEP : The button labeled Visibility Hidden should not be displayed");
-		Assert.assertTrue(wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[@id='invisibleButton']"))));
-	
-		System.out.println("STEP : The button labeled Display None should not be displayed");
-		Assert.assertTrue(wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//button[@id='notdisplayedButton']"))));
-	
+		System.out.println("VERIFY : The button labeled Visibility Hidden should not be displayed");
+		Assert.assertFalse(invisibleButton.isDisplayed(), "invisibleButton is visible");
+		
+		System.out.println("VERIFY : The button labeled Display None should not be displayed");
+		Assert.assertFalse(notdisplayedButton.isDisplayed(), "notdisplayedButton is visible");
+		
 	}
 }
